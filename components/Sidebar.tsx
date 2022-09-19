9663af6d68ffa6ef1c2665be0b3b948649acaf5b
Breldan Muthaka <breldanmuthaka@Breldans-MacBook-Pro.local>
2022-09-19 23:40:58 +0300

    Publishing videos

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -12,7 +12,6 @@ import SuggestedAccounts from "./SuggestedAccounts";
 const Sidebar = () => {
   const [showSidebar, setShowSidebar] = useState(true);
   const { pathname } = useRouter();
-  const userProfile = false;
   const activeLink =
     "flex items-center gap-3 hover:bg-primary p-3 justify-center xl:justify-start cursor-pointer font-semibold text-[#F51997] rounded";
 
@@ -40,30 +39,6 @@ const Sidebar = () => {
               </div>
             </Link>
           </div>
-          {!userProfile && (
-            <div className="px-2 py-4 hidden xl:block">
-              <p className="text-gray-400">
-                Login to comment and like on videos
-              </p>
-              <div className="pr-4">
-                <GoogleLogin
-                  clientId=""
-                  render={(renderProps) => (
-                    <button
-                      className="bd-white text-lg text-[#F51997] border-[1px] border-[#F51997] font-semibold px-6 py-3 rounded-md outline-none w-full mt-3  hover:text-white hover:bg-[#F51997] cursor-pointer"
-                      onClick={renderProps.onClick}
-                      disabled={renderProps.disabled}
-                    >
-                      Login
-                    </button>
-                  )}
-                  onSuccess={() => {}}
-                  onFailure={() => {}}
-                  cookiePolicy="single_host_origin"
-                />
-              </div>
-            </div>
-          )}
           <Discover />
           <SuggestedAccounts />
           <Footer />
--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: [
+      "drive.google.com",
+      "unsplash.com",
+      "lh3.googleusercontent.com",
+      "www.instagram.com",
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
