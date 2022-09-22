6c0e1861f5066ed82c45450dc11bc92cf2be294c
Breldan Muthaka <breldanmuthaka@Breldans-MacBook-Pro.local>
2022-09-23 00:11:59 +0300

    Search Filter User Profiles Comments Likes

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import NoResults from "../components/NoResults";
 import VideoCard from "../components/VideoCard";
 import { Video } from "../types";
+import { BASE_URL } from "../utils";
 
 interface IProps {
   videos: Video[];
@@ -21,9 +22,18 @@ const Home = ({ videos }: IProps) => {
   );
 };
 
-export const getServerSideProps = async () => {
-  const { data } = await axios.get(`http://localhost:3000/api/post`);
-  return { props: { videos: data } };
+export const getServerSideProps = async ({
+  query: { topic },
+}: {
+  query: { topic: string };
+}) => {
+  let response;
+  if (topic) {
+    response = await axios.get(`${BASE_URL}/api/discover/${topic}`);
+  } else {
+    response = await axios.get(`${BASE_URL}/api/post`);
+  }
+  return { props: { videos: response.data } };
 };
 
 export default Home;
--- pages/upload.tsx
+++ pages/upload.tsx
@@ -7,7 +7,8 @@ import useAuthStore from "../store/authStore";
 import { client } from "../utils/client";
 import { topics } from "../utils/constants";
 import axios from "axios";
-// import { BASE_URL } from "../utils";
+import { BASE_URL } from "../utils";
+// import { ${BASE_URL} } from "../utils";
 
 const Upload = () => {
   const [caption, setCaption] = useState("");
@@ -69,7 +70,7 @@ const Upload = () => {
         },
         topic,
       };
-      await axios.post(`http://localhost:3000/api/post`, document);
+      await axios.post(`${BASE_URL}/api/post`, document);
       router.push("/");
     }
   };
